7e126274f6d7a492d8bef78be945b44e18945569
Mosawer Mahboob <46627371+masomahboob@users.noreply.github.com>
2022-09-05 00:06:52 +0000

    Fixed the check for years that are evenly divisible by 400.

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-	leapYearCalc(1)
+	leapYearCalc(0)
 
 }
 
@@ -14,7 +14,7 @@ func leapYearCalc(choice int) []int {
 
 	var result []int
 
-  // determin if a given year is a leap year
+	// determin if a given year is a leap year
 	if choice == 0 {
 
 		var year int
@@ -23,13 +23,14 @@ func leapYearCalc(choice int) []int {
 
 		fmt.Scan(&year)
 
-		if year%4 == 0 && year%100 != 0 {
+		if (year%400 == 0) || (year%4 == 0 && year%100 != 0) {
 			result = append(result, year)
 			fmt.Printf("%d is a leap year.\n", year)
 		} else {
 			fmt.Printf("%d is not a leap year.\n", year)
 		}
-    // determin multiple years to be leap year or not
+
+		// determin multiple years to be leap year or not
 	} else if choice == 1 {
 
 		startingYear := 2000
@@ -41,7 +42,7 @@ func leapYearCalc(choice int) []int {
 		fmt.Scan(&upToYear)
 
 		for y := startingYear; y <= upToYear; y++ {
-			if y%4 == 0 && y%100 != 0 {
+			if (y%400 == 0) || (y%4 == 0 && y%100 != 0) {
 				result = append(result, y)
 			}
 		}
